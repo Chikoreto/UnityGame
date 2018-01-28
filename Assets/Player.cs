26bdcf86c647c35b159e3a1efe37888524ca7331
Chikoreto <35885776+Chikoreto@users.noreply.github.com>
2018-01-28 16:20:55 +0900

    1st commit
    キャラ移動

---
+++ Assets/Player.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//設定したアニメフラグの名前
+public enum AnimeKeyFlag
+{
+    keyjump,
+    keyRum,
+}
+
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] private Vector3 velocity;  //移動方向
+
+    [SerializeField] private float moveSpeed = 5.0f; //移動速度
+
+    [SerializeField] private float applySpeed = 0.2f;       //回転の適用速度
+
+    [SerializeField] private PlayerFollowCamera refCamera;  //カメラ水平回転参照
+
+    private Animator animator;      //アニメコンポーネント
+
+
+    //初期化
+    void Start()
+    {
+        //自分に設定されているアニメコンポーネント取得
+        this.animator = GetComponent<Animator>();
+    }
+	
+	//更新
+	void Update () {
+        //WASDの入力でZX面を移動する方向を決定
+        velocity = Vector3.zero;
+        if(Input.GetKey(KeyCode.W))         //run_F
+        {
+            velocity.z += 1;
+        }
+        if (Input.GetKey(KeyCode.S))        //run_F
+        {
+            velocity.z -= 1;
+            
+        }
+        if (Input.GetKey(KeyCode.D))       //run_R
+        {
+            velocity.x += 1;
+        }
+        if (Input.GetKey(KeyCode.A))        //run_L
+        {
+            velocity.x -= 1;
+        }
+
+            //1秒でmoveSpeed分進むように調整
+            velocity = velocity.normalized * moveSpeed * Time.deltaTime;
+
+        //いずれかの方向に移動している場合
+        if (velocity.magnitude > 0)
+        {
+
+            //プレイヤー回転
+            //無回転時のプレイヤー高等部をカメラの水平回転で回した移動の方向にだんだん回して近づける
+            transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.LookRotation(refCamera.hRotation * velocity),applySpeed);
+
+            //アニメーション設定
+            this.animator.SetBool(AnimeKeyFlagStr.ObtainAnimeKeyFlag(AnimeKeyFlag.keyRum), true);
+
+            //プレイヤーの位置更新
+            //カメラの水平回転で回した移動方向を足す
+            transform.position +=  refCamera.hRotation * velocity;
+        }
+        else
+        {
+            this.animator.SetBool(AnimeKeyFlagStr.ObtainAnimeKeyFlag(AnimeKeyFlag.keyRum), false);
+
+        }
+
+    }
+}
+
+public static class AnimeKeyFlagStr
+{
+    public static string ObtainAnimeKeyFlag(this AnimeKeyFlag flag)
+    {
+        string[] valuse = {"isJump","isRun" };
+        return valuse[(int)flag];
+    }
+}
---
+++ Assets/PlayerFollowCamera.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollowCamera : MonoBehaviour {
+
+    [SerializeField] private Transform Player;              //注視対象
+
+    [SerializeField] private float distance = 15.0f;        //注視対象からカメラを離す距離
+    [SerializeField] public Quaternion vRotation;          //カメラの垂直回転
+    [SerializeField] public Quaternion hRotation;          //カメラの水平回転
+    [SerializeField] private float turnSpeed = 5.0f;
+
+
+
+
+    // Use this for initialization
+    void Start () {
+        //回転初期化
+        vRotation = Quaternion.Euler(30, 0, 0);             //垂直回転はX方向30度(見下ろし)
+        hRotation = Quaternion.identity;                    //水平回転はなし
+        transform.rotation = hRotation * vRotation;         //最終的なカメラ角度。垂直回転後水平回転
+
+        //位置の初期化
+        //playerの位置から距離distance手前に引いた値を設定
+        transform.position = Player.position - transform.rotation * Vector3.forward * distance;
+
+	}
+	
+	void LateUpdate () {
+
+        //マウスの移動に応じて回転(水平)
+        hRotation *= Quaternion.Euler(0,Input.GetAxis("Mouse X")*turnSpeed,0);
+
+
+
+        // カメラの回転(transform.rotation)の更新
+        // 方法1 : 垂直回転してから水平回転する合成回転とします
+        transform.rotation = hRotation * vRotation;
+
+        //位置の初期化
+        //playerの位置から距離distance手前に引いた値を設定
+        transform.position = Player.position - transform.rotation * Vector3.forward * distance;
+
+    }
+}
